9886909f0d149b02a056093857a066f4b156a47b
mihai10001 <rvivits@gmail.com>
2022-02-28 12:46:26 +0200

    add main layout protected route

--- reservation_system/src/app/app-routing.module.ts
+++ reservation_system/src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'login',
     component: AuthComponent,
+  },
+  {
+    path: '',
+    loadChildren: () => import('./main-layout/main-layout.module').then(m => m.MainLayoutModule),
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard]
   }
 ];
 
