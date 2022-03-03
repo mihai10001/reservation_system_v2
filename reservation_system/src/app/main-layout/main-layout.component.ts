30ef93bc91921be59c535bebd0c6b34a744ed896
mihai10001 <rvivits@gmail.com>
2022-03-03 12:07:18 +0200

    add sidebar with all pages links

--- reservation_system/src/app/main-layout/main-layout.component.ts
+++ reservation_system/src/app/main-layout/main-layout.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss'],
 })
-export class MainLayoutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class MainLayoutComponent{
+  constructor(private authService: AuthService) { }
+  logout() { this.authService.logout() }
 }
